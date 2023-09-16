11cdaba1a5e56fd6137e21f41bc6f4b8e1407536
Rowan GENG <rowan.gw@outlook.com>
2023-09-16 22:18:42 +0800

    ccfddl-cli: suport multiple filter conditions
    - parse multiple fitlers
    - add help info for args
    - minor change for filter logic
    
    Fix [Issue #302](https://github.com/ccfddl/ccf-deadlines/issues/302)

--- cli/ccfddl/__main__.py
+++ cli/ccfddl/__main__.py
@@ -1,3 +1,4 @@
+import string
 import requests
 import yaml
 from argparse import ArgumentParser
@@ -20,15 +21,23 @@ def parse_tz(tz):
 
 def parse_args():
     parser = ArgumentParser(description="cli for ccfddl")
-    parser.add_argument("--conf", type=str)
-    parser.add_argument("--sub", type=str)
-    parser.add_argument("--rank", type=str)
-    return parser.parse_args()
+    parser.add_argument("--conf", type=str, nargs='+', 
+                        help="A list of conference ids you want to filter, e.g.: '--conf CVPR ICML'")
+    parser.add_argument("--sub", type=str, nargs='+', 
+                        help="A list of subcategories ids you want to filter, e.g.: '--sub AI CG'")
+    parser.add_argument("--rank", type=str, nargs='+', 
+                        help="A list of ranks you want to filter, e.g.: '--rank C N'")
+    args = parser.parse_args()
+    # Convert all arguments to lowercase
+    for arg_name in vars(args):
+        arg_value = getattr(args, arg_name)
+        if arg_value:
+            setattr(args, arg_name, [arg.lower() for arg in arg_value])
+    return args
 
 
 def main():
     args = parse_args()
-
     yml_str = requests.get(
         "https://ccfddl.github.io/conference/allconf.yml").content.decode("utf-8")
     all_conf = yaml.safe_load(yml_str)
@@ -59,18 +68,30 @@ def main():
 
     all_conf_ext = sorted(all_conf_ext, key=lambda x: x['time_obj'])
 
+    # This is not an elegant solution. 
+    # The purpose is to keep the above logic untouched, 
+    # return alpha id(conf name) without digits(year)
+    def alpha_id(with_digits:string) -> string:
+        return ''.join(char for char in with_digits.lower() if char.isalpha())
+
     table = [["title", "sub", "rank", "ddl", "link"]]
+    # Filter intersection by args
     for x in all_conf_ext:
         skip = False
-        if args.conf and args.conf.lower() not in x["id"].lower():
+        if args.conf and alpha_id(x["id"]) not in args.conf:
             skip = True
-        if args.sub and args.sub.lower() not in x["sub"].lower():
+        if args.sub and x["sub"] not in args.sub:
             skip = True
-        if args.rank and args.rank.lower() not in x["rank"].lower():
+        if args.rank and x["rank"] not in args.rank:
             skip = True
         if not skip:
-            table.append([x["title"], x["sub"], x["rank"],
-                          str(x["time_obj"] - now), x["link"]])
+            table.append(
+                [x["title"], 
+                 x["sub"], 
+                 x["rank"],
+                 str(x["time_obj"] - now), 
+                 x["link"]]
+                )
 
     print(tabulate(table, headers='firstrow', tablefmt='fancy_grid'))
 
