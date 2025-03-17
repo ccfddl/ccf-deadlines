d0d753f01846b1687a984a833bf6116510e39052
兮尘 <23211526+LinXueyuanStdio@users.noreply.github.com>
2025-03-18 01:19:27 +0800

    [feature] fine-grained iCal subscription by category

--- cli/ccfddl/convert_to_ical.py
+++ cli/ccfddl/convert_to_ical.py
@@ -1,8 +1,10 @@
 import yaml
-from icalendar import Calendar, Event, Timezone, TimezoneStandard
 import re
-from datetime import datetime, timedelta, timezone
 import uuid
+from collections import defaultdict
+from itertools import combinations
+from datetime import datetime, timedelta, timezone
+from icalendar import Calendar, Event, Timezone, TimezoneStandard
 
 # 中英类别映射表
 def load_mapping(path: str="conference/types.yml"):
@@ -180,9 +182,48 @@ def convert_to_ical(file_paths: list[str], output_path: str, lang: str='en', SUB
     with open(output_path, 'wb') as f:
         f.write(cal.to_ical())
 
+
+def reverse_index(file_paths: list[str], subs: list[str]):
+    index = defaultdict(list)
+
+    result = []
+    # 遍历所有可能的选择数量，从 1 到总数量
+    for r in range(1, len(subs)):
+        # 生成所有长度为 r 的组合
+        combos = combinations(subs, r)
+        for combo in combos:
+            # 将组合转换为字符串并添加到结果列表中
+            result.append('_'.join(combo))
+    # 10 个类别共有 1022 种组合（去掉空集和全集）
+
+    for file_path in file_paths:
+        with open(file_path, 'r') as f:
+            conferences = yaml.safe_load(f)
+
+        for conf_data in conferences:
+            title = conf_data['title']
+            sub = conf_data['sub']
+            rank = conf_data['rank']
+
+            # index[title].append(file_path)
+            index['ccf_' + rank.get('ccf', 'N')].append(file_path)
+            index['core_' + rank.get('core', 'N')].append(file_path)
+            index['thcpl_' + rank.get('thcpl', 'N')].append(file_path)
+            for name in result:
+                if sub in name:
+                    index['sub_' + name].append(file_path)
+
+    return index
+
+
 if __name__ == '__main__':
-    from xlin.util import ls
+    from xlin import ls, element_mapping
     SUB_MAPPING = load_mapping("conference/types.yml")
     paths = ls("conference", filter=lambda f: f.name != "types.yml")
     convert_to_ical(paths, 'deadlines_zh.ics', 'zh', SUB_MAPPING)
     convert_to_ical(paths, 'deadlines_en.ics', 'en', SUB_MAPPING)
+    index = reverse_index(paths, list(SUB_MAPPING.keys()))
+    for lang in ['zh', 'en']:
+        f = lambda key: (True, convert_to_ical(index[key], f'deadlines_{lang}_{key}.ics', lang, SUB_MAPPING))
+        element_mapping(index.keys(), f)
+    print("转换完成")
