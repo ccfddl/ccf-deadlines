80f3591bac16bf0fb7e48c8bf9b202512ec97427
Zhihao Chen <jacklightchen@gmail.com>
2026-09-26 16:33:04 +0800

    Shorten the conference card hint

--- src/components/showtable.rs
+++ src/components/showtable.rs
@@ -866,7 +866,7 @@ pub fn ShowTable(use_english: RwSignal<bool>) -> impl IntoView {
             <div class="conference-list">
                 <div class="conference-list-hint">
                     {move || if use_english.get() {
-                        "Click a conference card for more information."
+                        "Click over cells for more information."
                     } else {
                         "点击会议卡片查看更多信息。"
                     }}
